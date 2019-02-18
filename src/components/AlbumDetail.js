8825af4358c8b147ddf059519018671fe9cdfe8b
Stanley Louis <stanlouis@msn.com>
2019-02-18 10:48:59 -0500

    Add AlbumDetail, Card and CardSection components

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+
+const AlbumDetail = props => {
+  const { album } = props;
+  return (
+    <Card>
+      <CardSection>
+        <Text>{album.title}</Text>
+      </CardSection>
+    </Card>
+  );
+};
+
+export default AlbumDetail;
---
+++ src/components/CardSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View } from 'react-native';
+const CardSection = props => {
+  return <View style={style.containerStyle}>{props.children}</View>;
+};
+
+const style = {
+  containerStyle: {
+    borderBottomWith: 1,
+    padding: 5,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    borderColor: '#ddd',
+    position: 'relative',
+  },
+};
+export default CardSection;
